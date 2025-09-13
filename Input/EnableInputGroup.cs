576a0e8274ee60d294434381ca8d4ae00240a57d
DarkMorninG <Bustercall@gmx.net>
2025-09-13 08:56:16 +0200

    auto enabled input

---
+++ Input/EnableInputGroup.cs
@@ -0,0 +1,23 @@
+﻿using DBH.Attributes;
+using DBH.Input.api.Extending;
+using DBH.UI.Controller;
+
+namespace DBH.UI.Input {
+    [Bean]
+    public class EnableInputGroup : IMenuInteractionChanged{
+        [Grab]
+        private IInputController inputController;
+        
+        public int Order() {
+            return 0;
+        }
+
+        public void Enabled() {
+            inputController.EnableGroup("UI");
+        }
+
+        public void Disabled() {
+            inputController.DisableGroup("UI");
+        }
+    }
+}
