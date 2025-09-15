cdc314abd5e45d8a7921dcd8dae3dccdb9fdf578
DarkMorninG <Bustercall@gmx.net>
2025-09-15 22:58:18 +0200

    delay the input for incanddec in menus

--- Input/IncDecInput.cs
+++ Input/IncDecInput.cs
@@ -1,6 +1,7 @@
 ﻿using DBH.Attributes;
 using DBH.Input.api.Extending;
 using UnityEngine;
+using Vault.BetterCoroutine;
 
 namespace DBH.UI.Input {
     [Bean]
@@ -8,6 +9,7 @@ namespace DBH.UI.Input {
         public delegate void IncDecPressed(Direction direction);
 
         public event IncDecPressed OnIncDecPressed;
+        private IAsyncRuntime inputDelay;
 
         public override string MappedName() {
             return "IncDec";
@@ -16,8 +18,14 @@ namespace DBH.UI.Input {
         protected override void InputChanged(Vector2 input) {
             var inputPressed = input.x > .5f || input.y > .5f || input.x > -.5f || input.y > -.5f;
             if (!inputPressed) return;
-            OnIncDecPressed?.Invoke(GetDirection(input));
+            var direction = GetDirection(input);
+            if (inputDelay.IsNotRunning()) {
+                inputDelay = AsyncRuntime.WaitForSeconds(() => OnIncDecPressed?.Invoke(direction), .2f);
+            } else {
+                inputDelay.AndAfterFinishDo(() => inputDelay = AsyncRuntime.WaitForSeconds(() => OnIncDecPressed?.Invoke(direction), .2f));
+            }
         }
+
 
         private Direction GetDirection(Vector2 input) {
             return input.y switch {
