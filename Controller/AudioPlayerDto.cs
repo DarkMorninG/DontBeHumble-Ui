122a873d8ab07cd88b43e673b257cb2824b9bb6f
DarkMorninG <Bustercall@gmx.net>
2025-10-06 21:51:12 +0200

    commit sound only excuted one time

--- Controller/AudioPlayerDto.cs
+++ Controller/AudioPlayerDto.cs
@@ -3,17 +3,22 @@
 namespace DBH.UI.Controller {
     public class AudioPlayerDto {
         private AudioSource audioSource;
+        private bool hasPlayed;
 
         public AudioPlayerDto(AudioSource audioSource) {
             this.audioSource = audioSource;
         }
 
         public void Play() {
+            if (hasPlayed) return;
             audioSource.Play();
+            hasPlayed = true;
         }
 
         public void Play(AudioClip audioClip) {
+            if (hasPlayed) return;
             audioSource.PlayOneShot(audioClip);
+            hasPlayed = true;
         }
     }
 }
